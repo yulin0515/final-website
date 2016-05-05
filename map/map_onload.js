b3e59de22a90b9b6073c4dee0b2429bf11ddd4de
yulin0515 <ly25@imail.iu.edu>
2016-05-05 15:55:12 -0400

    ff

---
+++ map/map_onload.js
@@ -0,0 +1,38 @@
+$(function(){
+		
+		$("#custom_places").mapsed({
+			showOnLoad: 	
+			[
+				
+				{
+					//autoShow: true,
+					//canEdit: false,
+					lat: 21.171875,
+					lng: -86.874885,
+					//name: "世界之窗",
+					//street: "世界奇观！",
+					userData: 2
+				},
+				{
+					//autoShow: true,
+					//canEdit: false, 
+					lat: 21.156507,
+					lng: -86.877459,
+					//name: "北京",
+					//street: "西直门欢迎你！",
+					userData: 3
+				},
+				{
+					//autoShow: true,
+					//canEdit: false,
+					lat: 21.146007,
+					lng: -86.507459,
+					//name: "锦绣中华民俗文化村",
+					//street: "邀你遨游最美赛花节！",
+					userData: 3
+				}
+			]
+			
+		});	
+
+	});
